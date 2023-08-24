0b7b1e034193b1d57b6fdb0d8ecc674192ceb19e
SharanaBasava022 <59363468+SharanaBasava022@users.noreply.github.com>
2023-08-24 06:40:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-const port = 5005
+const port = 5000
 
 app.get('/', (req, res) => {
-    res.send('Hiiiiiii Hello World!')
+    res.send('Hi Hello World!')
 })
 
 app.listen(port, () => {
